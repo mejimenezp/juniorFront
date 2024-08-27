11976a519a9728274b7912a29b0703a4d398fa2b
mejimenezp <143832231+mejimenezp@users.noreply.github.com>
2024-08-26 21:55:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/dashboard'; 
+import { AuthProvider } from './context/AuthProvider';
+import PrivateRoute from './services/PrivateRoute';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />               
+                 </Routes>
+            </AuthProvider>
+            <ToastContainer />
+        </Router>
+    );
 }
 
 export default App;
